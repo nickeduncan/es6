1a23aac36112ec50eb0db7bda8e7e3e3def13aaa
Nick Duncan <nickeduncan@gmail.com>
2017-05-02 23:10:44 -0400

    used destructuring assignment to set variables to the index value of an array and key-value of an object

--- app/index.js
+++ app/index.js
@@ -1,5 +1,26 @@
-function collect(...a) {
-  console.log(a);
-}
+// let z = [4, 5, 6];
+// let four = z[0];
+// let five = z[1];
+// console.log(four, five);
+//
+// let z = [4, 5, 6];
+// let[four, five] = z;
+// console.log(four, five);
 
-collect(3, 6, 9, 12, 15);
+// let animals = ["Simba", "Zazu", "Ed"];
+// let [lion, bird] = animals;
+// console.log(lion, bird);
+
+// let king = { name: 'Mufasa', kids: 1 };
+// let name = king.name;
+// let kids = king.kids;
+// console.log(name, kids);
+
+// let king = { name: 'Mufasa', kids: 1 };
+// let { name, kids } = king;
+// console.log(name, kids);
+
+let son = { name: 'Simba', parents: 2 };
+let name, parents;
+({ name, parents } = son);
+console.log(name, parents);
