b254551fcf7919bfb0474d37c669229c19ed0c6e
Nick Duncan <nickeduncan@gmail.com>
2017-05-03 17:53:13 -0400

    accessed a local variable inside a function using a closure

--- app/index.js
+++ app/index.js
@@ -1,31 +1,20 @@
-// let a = new Map();
-// let key_1 = "string key";
-// let key_2 = { a: 'key'};
-// let key_3 = function() {};
-//
-// a.set(key_1, 'return value for a string key');
-// a.set(key_2, 'return value for an object key');
-// a.set(key_3, 'return value for a function key');
-// console.log(a);
-
-// let numArr = [[1, 'one'], [2, 'two'], [3, 'three']];
-// let valMap = new Map(numArr);
-// // console.log(valMap);
-//
-// for (let [key, value] of valMap.entries()) {
-//   console.log(`${key} points to ${value}`);
+// let call = () => {
+//   let secret = 'ES6 rocks!';
+//   let reveal = () => {
+//     console.log(secret);
+//   }
+//   reveal();
 // }
+//
+// call();
 
-let string = 'asldfjweoqpieworasasdlkjweoinxcvnm';
-
-let letters = new Map();
-for (let i=0; i<string.length; i++) {
-  let letter = string[i];
-  if (!letters.has(letter)) {
-    letters.set(letter, 1);
-  } else {
-    letters.set(letter, letters.get(letter) + 1);
+let call = () => {
+  let secret = 'ES6 rocks!';
+  let reveal = () => {
+    console.log(secret);
   }
+  return reveal;
 }
 
-console.log(letters);
+let unveil = call();
+unveil();
