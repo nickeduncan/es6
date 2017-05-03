36ed0145226cace6fb711c5d27367938375585ba
Nick Duncan <nickeduncan@gmail.com>
2017-05-03 11:25:08 -0400

    created a child class of animal (Lion) with an additional parameter and revised function, and imported Animal from a module

--- app/fellowship.js
+++
@@ -1,3 +0,0 @@
-const fellowship = ['Frodo', 'Samwise', 'Gandalf'];
-const total = fellowship.length;
-export{ fellowship, total };
--- app/index.js
+++ app/index.js
@@ -1,14 +1,31 @@
-class Animal {
-  constructor(name, height) {
-    this.name = name;
-    this.height = height;
+import Animal from './Animal.js';
+
+// class Animal {
+//   constructor(name, height) {
+//     this.name = name;
+//     this.height = height;
+//   }
+//
+//   hello() {
+//     console.log(`Hi! I'm ${this.name} from the Animal kingdom`);
+//   }
+// }
+
+// let king = new Animal("Mufasa", 4.5);
+// // console.log(king);
+// king.hello();
+
+class Lion extends Animal {
+  constructor(name, height, color) {
+    super(name, height);
+    this.color = color;
   }
 
   hello() {
-    console.log(`Hi! I'm ${this.name} from the Animal kingdom`);
+    console.log(`Hi! I'm ${this.name} from Pride Rock!`);
   }
 }
 
-let king = new Animal("Mufasa", 4.5);
-
-king.hello();
+let son = new Lion("Simba", 2, "golden");
+// console.log(son);
+son.hello();
