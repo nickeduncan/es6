df68946457f13a5b173353e8259179195380df4d
Nick Duncan <nickeduncan@gmail.com>
2017-05-02 23:50:56 -0400

    used filter and arrow function to select element in array

--- app/index.js
+++ app/index.js
@@ -18,7 +18,21 @@
 
 
 
-let values = [20, 30, 40];
+// let values = [20, 30, 40];
+//
+// let doubled = values.map((n) => n*2);
+// console.log(doubled);
 
-let doubled = values.map((n) => n*2);
-console.log(doubled);
+
+
+// let points = [7, 16, 21, 4, 3, 22, 5];
+// let highScores = points.filter((n) => {
+//   return n > 15;
+// })
+// console.log(highScores);
+
+
+
+// let points = [7, 16, 21, 4, 3, 22, 5];
+// let highScores = points.filter((n) => n > 15)
+// console.log(highScores);
