2ed68d479824af84ef3b070b33bb289d63c15743
Nick Duncan <nickeduncan@gmail.com>
2017-05-03 17:10:12 -0400

    used Set to create arrays of with unique values

--- app/index.js
+++ app/index.js
@@ -1,24 +1,23 @@
-function Wizard(name, house, pet) {
-  this.name = name;
-  this.house = house;
-  this.pet = pet;
+// let a = new Set();
+// a.add(5);
+// a.add(43);
+// a.add("Woohoo");
+// a.add({x: 50, y: 200});
+// // console.log(a);
+//
+// // console.log(a.size);
+//
+// console.log(a.has(7));
 
-  this.greet = () => `I'm ${this.name} from ${this.house}`;
-}
-
-Wizard.prototype.pet_name;
-
-// Wizard.prototype.info = () => {
-//   return `I have a ${this.pet} named ${this.pet_name}`;
+// let numbers = [5, 7, 8, 13, 17];
+// let numSet = new Set(numbers);
+// // console.log(numSet);
+//
+// for (let element of numSet.values()) {
+//   console.log(element);
 // }
 
-// Need function keyword to access prototype's "this"
-Wizard.prototype.info = function() {
-  return `I have a ${this.pet} named ${this.pet_name}`;
-}
-
-let harry = new Wizard("Harry Potter", "Gryffindor", "Owl");
-harry.pet_name = "Hedwig";
-console.log(harry);
-// console.log(harry.greet());
-console.log(harry.info());
+let chars = 'asdklfjlkasjdflkjasdlkfjdlkasdflkjweoe'
+let chars_arr = chars.split("");
+let chars_set = new Set(chars_arr);
+console.log(chars_set);
