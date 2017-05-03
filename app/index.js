229a1fd80bd6fb74582bae3efebd627100a74b2d
Nick Duncan <nickeduncan@gmail.com>
2017-05-03 18:19:05 -0400

    created a modular object function with exposed getter and setter methods for local variables

--- app/index.js
+++ app/index.js
@@ -1,33 +1,25 @@
-// const addSuffix = (x) => {
-//   const concat = (y) => {
-//     return y+ x;
-//   }
-//   return concat;
-// }
-//
-// // let add_ness = addSuffix("ness");
-// // console.log(add_ness);
-// // let h = add_ness("happi");
-// // console.log(h);
-//
-// let add_ful = addSuffix("ful");
-// let f = add_ful("fruit");
-// console.log(f);
+const budget = () => {
+  let balance = 0;
+  let changeBal = (val) => {
+    return balance += val;
+  }
 
-// const product = (x) => {
-//   return y => {
-//     return y * x;
-//   }
-// }
-//
-// let mult5 = product(5);
-// // console.log(mult5(3));
-// let double = product(2);
-// console.log(double(9));
+  const deposit20 = () => changeBal(20);
+  const withdraw20 = () => changeBal(-20);
+  const check = () => balance;
 
-const product = x => y => y * x;
+  return {
+    deposit20,
+    withdraw20,
+    check
+  }
+}
 
-let mult5 = product(5);
-// console.log(mult5(3));
-let double = product(2);
-console.log(double(9));
+let wallet = budget();
+// console.log(wallet);
+wallet.deposit20();
+wallet.withdraw20();
+wallet.deposit20();
+wallet.deposit20();
+console.log(wallet.check());
+// console.log(wallet.balance);
