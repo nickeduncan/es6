2bf22c7e475d91f6bb4b009db7124f4ad8ad1ef9
Nick Duncan <nickeduncan@gmail.com>
2017-05-04 14:12:07 -0400

    created async function to await the running of two other async functions

--- app/index.js
+++ app/index.js
@@ -34,13 +34,6 @@ async function async_two() {
   return "two"
 }
 
-async function async_three() {
-  const one = await async_one();
-  console.log(one);
-  const two = await async_two();
-  console.log(two);
-}
-
 async function async_four() {
   const [res_one, res_two] = await Promise.all(
     [async_one(), async_two()]
