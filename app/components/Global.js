a73e015ced2bb77832a47500facea314182d525a
Nick Duncan <nickeduncan@gmail.com>
2017-05-04 15:42:46 -0400

    added global app component that render in index.js --> index.html

---
+++ app/components/Global.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+
+// class Global extends Component {
+//   render() {
+//     return (
+//       <div>The Global App Component</div>
+//     )
+//   }
+// }
+
+class Global extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Book Explorer!</h2>
+      </div>
+    )
+  }
+}
+
+export default Global;
--- app/index.js
+++ app/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Global from './components/Global';
+
+// ReactDOM.render(
+//   <div>React Application!</div>, document.getElementById('root')
+// );
 
 ReactDOM.render(
-  <div>React Application!</div>, document.getElementById('root')
+  <Global />, document.getElementById('root')
 );
