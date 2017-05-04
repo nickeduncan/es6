0d7a7a26f30d1a72416cc21cb0bd7864b19e6e23
Nick Duncan <nickeduncan@gmail.com>
2017-05-03 21:53:04 -0400

    created iterator to loop through arguments in an array one at a time

--- app/index.js
+++ app/index.js
@@ -1,17 +1,48 @@
-function* evens() {
-  let count = 0;
-  while(true) {
-    count += 2;
-    let reset = yield count;
-    if (reset) {
-      count = 0;
-    }
-  }
+// const arrayIterator = (array) => {
+//   let index = 0;
+//
+//   return {
+//     next: () => {
+//       if (index < array.length) {
+//         let next = array[index];
+//         index += 1;
+//         return next;
+//       }
+//     }
+//   }
+// }
+//
+
+
+// let it = arrayIterator([1, 2, 3]);
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
+
+// function* arrayIterator() {
+//   yield arguments;
+// }
+//
+// var it = arrayIterator(1, 2, 3);
+// console.log(it.next().value);
+
+// function* arrayIterator() {
+//   for (let arg of arguments) {
+//     yield arg;
+//   }
+// }
+//
+// var it = arrayIterator(1, 2, 3);
+// console.log(it.next().value);
+// console.log(it.next().value);
+// console.log(it.next().value);
+
+function* arrayIterator() {
+    yield* arguments;
 }
 
-let sequence = evens();
-console.log(sequence.next().value);
-console.log(sequence.next().value);
-console.log(sequence.next().value);
-console.log(sequence.next(true).value);
-console.log(sequence.next().value);
+var it = arrayIterator(1, 2, 3);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
