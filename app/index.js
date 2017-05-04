6d6dc4a54d44e610eafe0589135e17ea4d9a7e99
Nick Duncan <nickeduncan@gmail.com>
2017-05-03 21:40:00 -0400

    used reset to reset while loop in generator gpa

--- app/index.js
+++ app/index.js
@@ -1,25 +1,17 @@
-// function* letterMaker() {
-//   yield 'a';
-//   yield 'b';
-//   yield 'c';
-// }
-//
-// let letterGen = letterMaker();
-// console.log(letterGen.next().value);
-// console.log(letterGen.next().value);
-// console.log(letterGen.next().value);
-// console.log(letterGen.next().value);
-
-function* countMaker() {
+function* evens() {
   let count = 0;
-  // while (count < 3) {
-  while (count < 4) {
-    yield count += 1;
+  while(true) {
+    count += 2;
+    let reset = yield count;
+    if (reset) {
+      count = 0;
+    }
   }
 }
 
-let countGen = countMaker();
-console.log(countGen.next().value);
-console.log(countGen.next().value);
-console.log(countGen.next().value);
-console.log(countGen.next().value);
+let sequence = evens();
+console.log(sequence.next().value);
+console.log(sequence.next().value);
+console.log(sequence.next().value);
+console.log(sequence.next(true).value);
+console.log(sequence.next().value);
