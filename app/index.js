cc2b801b9424eaf1aa31e9e0ea2166e314ce45ce
Nick Duncan <nickeduncan@gmail.com>
2017-05-04 15:12:56 -0400

    reconfigured app for react

--- app/index.js
+++ app/index.js
@@ -1,44 +1,6 @@
-// async function async_one() {
-//   return "one";
-// }
-//
-// async function async_two() {
-//   throw new Error('Issue with async!');
-// }
-//
-// // async_one().then(response => console.log(response));
-// async_two().catch(error => console.log(error));
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// async function async_one() {
-//   return "one";
-// }
-//
-// async function async_two() {
-//   return "two"
-// }
-//
-// async function async_three() {
-//   const one = await async_one();
-//   console.log(one);
-//   const two = await async_two();
-//   console.log(two);
-// }
-//
-// async_three();
-
-async function async_one() {
-  return "one";
-}
-
-async function async_two() {
-  return "two"
-}
-
-async function async_four() {
-  const [res_one, res_two] = await Promise.all(
-    [async_one(), async_two()]
-  )
-  console.log(res_one, res_two);
-}
-
-async_four();
+ReactDOM.render(
+  <div>React Application!</div>, document.getElementById('root')
+);
